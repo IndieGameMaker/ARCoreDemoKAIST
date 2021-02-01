231176dcec8b0078f8c766903ac6cf92a4f11abf
Lee JaeHyun <myleje@gmail.com>
2021-02-01 14:21:34 +0900

    .

--- Assets/02.Scripts/HeroMgr.cs
+++ Assets/02.Scripts/HeroMgr.cs
@@ -11,12 +11,28 @@ public class HeroMgr : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        camTr = Camera.main.GetComponent<Transform>();
+        tr = GetComponent<Transform>();
+        anim  = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float distance = Vector3.Distance(camTr.position, tr.position);
+
+        if (distance <= 0.25f)
+        {
+            Debug.Log("Attack");
+        }
+
+        if (distance > 0.2f)
+        {
+            //회전 
+            Quaternion rot = Quaternion.LookRotation(camTr.position - tr.position);
+            tr.rotation = Quaternion.Slerp(tr.rotation, rot, Time.deltaTime * 10.0f);
+            //이동(전진)
+            tr.Translate(Vector3.forward * Time.deltaTime * 0.2f);
+        }
     }
 }
